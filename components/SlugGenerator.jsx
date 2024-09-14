44223f79d202af48139de672b0255c901ff16516
shiam-technext <141896624+shiam-technext@users.noreply.github.com>
2024-09-14 14:19:49 +0600

    feat: :sparkles: add slugify functionality

--- components/SlugGenerator.jsx
+++ components/SlugGenerator.jsx
@@ -1,6 +1,20 @@
+"use client";
+
 import { DocumentDuplicateIcon } from "@heroicons/react/24/solid";
+import slugify from "ez-slugify";
+import { useState } from "react";
 
 const SlugGenerator = () => {
+  const [textValue, setTextValue] = useState("");
+
+  const handleSlugify = () => {
+    if (typeof textValue !== "string" || !textValue.trim()) {
+      return;
+    }
+
+    return slugify(textValue);
+  };
+
   return (
     <>
       <label
@@ -11,8 +25,9 @@ const SlugGenerator = () => {
           id="search-bar"
           placeholder="Paste Your String to Generate Slug"
           className="px-6 py-2 w-full rounded-md flex-1 outline-none bg-white"
+          onChange={(e) => setTextValue(e.target.value)}
         />
-        <button className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all disabled:opacity-70">
+        <button className="w-full md:w-auto px-6 py-3 bg-black hover:bg-opacity-85 border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all disabled:opacity-70">
           <div className="flex items-center transition-all opacity-1 valid:">
             <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto">
               Slugify
@@ -21,13 +36,18 @@ const SlugGenerator = () => {
         </button>
       </label>
 
-      <div className="mt-8 mx-auto relative bg-white bg-opacity-100 min-w-sm md:w-1/2 max-w-7xl flex flex-col md:flex-row items-center justify-between border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300">
+      <div
+        className={
+          "mt-8 mx-auto relative bg-white bg-opacity-100 min-w-sm md:w-1/2 max-w-7xl flex flex-col md:flex-row items-center justify-between border py-2 px-2 rounded-2xl gap-2 shadow-2xl focus-within:border-gray-300 transition-opacity duration-300 ease-in-out" +
+          (textValue === "" ? " opacity-0" : " opacity-100")
+        }
+      >
         <p className="px-6 py-2 w-full rounded-md flex-1 outline-none">
-          currency-cent-pound-yen-currency
+          {handleSlugify()}
         </p>
 
-        <button className="w-full md:w-auto px-6 py-3 bg-black border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all disabled:opacity-70">
-          <span className="">
+        <button className="w-full md:w-auto px-6 py-3 bg-black hover:bg-opacity-85 border-black text-white fill-white active:scale-95 duration-100 border will-change-transform overflow-hidden relative rounded-xl transition-all disabled:opacity-70">
+          <span>
             <DocumentDuplicateIcon className="h-5 w-5" />
           </span>
         </button>
